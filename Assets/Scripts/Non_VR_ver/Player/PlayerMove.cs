6e8f43ef2a51ceaa60370e2c8b5856449e175723
5553ddd@naver.com <@galiano9>
2015-06-30 22:45:02 +0900

    Add Non VR Script

---
+++ Assets/Scripts/Non_VR_ver/Player/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : objectBase // 모든 오브젝트들은 objectBase를 상속
+{
+    [SerializeField]
+    private float m_fMoveSpeed;
+    [SerializeField]
+    private float m_fTurnSpeed;
+
+    private GameObject m_goCamera = null;
+
+    // 초기화에는 OnAwake, OnStart 사용
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+
+        m_goCamera = Camera.main.gameObject;
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        MoveProcess();
+    }
+
+    private void MoveProcess()
+    {
+        float fTransformZ = Input.GetAxis( "Vertical" ) * m_fMoveSpeed;
+        float fTransformX = Input.GetAxis( "Horizontal" ) * m_fMoveSpeed;
+
+        float fRotationX = Input.GetAxis( "Mouse X" ) * m_fTurnSpeed;
+        float fRotationY = Input.GetAxis( "Mouse Y" ) * m_fTurnSpeed;
+
+        fTransformZ *= Time.deltaTime;
+        fTransformX *= Time.deltaTime;
+
+        fRotationX *= Time.deltaTime;
+        fRotationY *= Time.deltaTime;
+
+        transform.Translate( fTransformX, 0, fTransformZ );
+        
+        transform.Rotate( 0, fRotationX, 0 );
+
+        m_goCamera.transform.Rotate( -fRotationY, 0, 0 );
+    }
+}
